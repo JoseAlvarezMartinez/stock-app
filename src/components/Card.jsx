9c57724dca6d92160a2774e5bc44136a764ff501
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-22 21:44:48 -0300

    -- Card Finalizada --

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,21 @@
 import styles from "./Card.module.css";
 import { useState } from "react";
+import { SlOptionsVertical } from "react-icons/sl";
 import CardImg from "../assets/cardImg.png";
 const Card = () => {
   const [cantidad, setCantidad] = useState(0);
   return (
     <div className={styles.card}>
+      <div className={styles.options}>
+        <SlOptionsVertical color="#fff" />
+      </div>
       <div className={styles.cardContainer}>
         <img
           src={CardImg}
           alt="Imagen del producto"
           className={styles.cardImg}
         />
+      <div className={styles.linea}></div>
         <h2 className={styles.tituloCard}>Nombre del Producto</h2>
 
         <div className={styles.contenedorCantidades}>
