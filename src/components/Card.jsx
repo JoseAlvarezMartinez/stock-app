25e4b6b54e98360bf699383d485522a0d3e6b486
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-22 21:39:54 -0300

    -- React Icons --

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,30 @@
 import styles from "./Card.module.css";
 import { useState } from "react";
+import CardImg from "../assets/cardImg.png";
 const Card = () => {
   const [cantidad, setCantidad] = useState(0);
   return (
     <div className={styles.card}>
-      <h2 className={styles.tituloCard}>Titulo</h2>
+      <div className={styles.cardContainer}>
+        <img
+          src={CardImg}
+          alt="Imagen del producto"
+          className={styles.cardImg}
+        />
+        <h2 className={styles.tituloCard}>Nombre del Producto</h2>
 
-      <div className={styles.contenedorCantidades}>
-        <button
-          disabled={cantidad == 0}
-          onClick={() => setCantidad((cantidad) => cantidad - 1)}
-        >
-          -
-        </button>
-        <p className={styles.cantidades}>{cantidad}</p>
-        <button onClick={() => setCantidad((cantidad) => cantidad + 1)}>
-          +
-        </button>
+        <div className={styles.contenedorCantidades}>
+          <button
+            disabled={cantidad == 0}
+            onClick={() => setCantidad((cantidad) => cantidad - 1)}
+          >
+            -
+          </button>
+          <p className={styles.cantidades}>{cantidad}</p>
+          <button onClick={() => setCantidad((cantidad) => cantidad + 1)}>
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
